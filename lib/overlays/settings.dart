708aa834eac1edf46930a708daa49521744ce2f1
Aleksi Alhola <aleksi.alhola@gmail.com>
2025-05-01 15:25:00 +0300

    add skeleton for pages

---
+++ lib/overlays/settings.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingsOverlay extends StatelessWidget {
+  const SettingsOverlay({super.key});
+
+  @override
+  Widget build(context) {
+    return const Text("Settings");
+  }
+}
---
+++ lib/views/calendar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({super.key});
+
+  @override
+  Widget build(context) {
+    return const Text("Calendar page");
+  }
+}
